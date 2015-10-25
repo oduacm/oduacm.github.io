7f70b616285b56236f28293fa2685df465008e98
Joshua Harris <joshua.jharris13@gmail.com>
2015-10-25 01:26:47 -0400

    added angular material theme

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+var app = angular.module('oduacm', ['ngMaterial']);
+
+app.controller('MainCtrl', function($scope, $timeout, $mdSidenav, $log){
+  $scope.toggleRight = buildToggler('right');
+  $scope.isOpenRight = function(){
+    return $mdSidenav('right').isOpen();
+  };
+  /**
+   * Supplies a function that will continue to operate until the
+   * time is up.
+   */
+  function debounce(func, wait, context) {
+    var timer;
+    return function debounced() {
+      var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+      $timeout.cancel(timer);
+      timer = $timeout(function() {
+        timer = undefined;
+        func.apply(context, args);
+      }, wait || 10);
+    };
+  }
+  function buildToggler(navID) {
+    return function() {
+      $mdSidenav(navID)
+        .toggle()
+        .then(function () {
+          $log.debug("toggle " + navID + " is done");
+        });
+    }
+  }
+});
+
+app.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function () {
+    $mdSidenav('right').close()
+      .then(function () {
+        $log.debug("close RIGHT is done");
+      });
+  };
+});
